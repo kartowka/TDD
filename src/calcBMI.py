b7ef8291e870f0d1d1c5f99f2caa58a655d2b9ba
kartowka <antonfley@gmail.com>
2020-01-13 09:34:14 +0200

    first commit folders/files

---
+++ src/calcBMI.py
@@ -0,0 +1,18 @@
+def bmicalc(weight,height):
+    return weight/height**2
+
+def BMIcheck(bmi):
+    if bmi>=18.5 and bmi<=24.9:
+        return "Healty range"
+    if bmi<18.5:
+        return "Under weight"
+    return "Over weight"
+
+
+def bubble_sort(nlist):
+    for i in range(len(nlist)):
+        for j in range(0,len(nlist)-i-1):
+            if nlist[j]>nlist[j+1]:
+                nlist[j], nlist[j+1] = nlist[j+1], nlist[j]
+
+
---
+++ test/calcBMI_test.py
@@ -0,0 +1,16 @@
+import unittest
+from src.calcBMI import *
+
+class bmitest(unittest.TestCase):
+    def test_bmicalc(self):
+        # stub
+        
+
+        # assume
+
+
+        # action
+
+
+        # expect/assert
+
