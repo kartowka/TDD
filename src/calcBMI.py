c23591203b7d9ecdd2dd3b1ca80d8bbd0ec4cc3a
kartowka <antonfley@gmail.com>
2020-01-13 10:47:06 +0200

    pushed

--- src/calcBMI.py
+++ src/calcBMI.py
@@ -1,18 +1,20 @@
-def bmicalc(weight,height):
-    return weight/height**2
+class BMI:
+    @staticmethod
+    def bmicalc(weight,height):
+        return weight/height**2
+    @staticmethod
+    def BMIcheck(bmi):
+        if bmi>=18.5 and bmi<=24.9:
+            return "Healty range"
+        if bmi<18.5:
+            return "Under weight"
+        return "Over weight"
 
-def BMIcheck(bmi):
-    if bmi>=18.5 and bmi<=24.9:
-        return "Healty range"
-    if bmi<18.5:
-        return "Under weight"
-    return "Over weight"
-
-
-def bubble_sort(nlist):
-    for i in range(len(nlist)):
-        for j in range(0,len(nlist)-i-1):
-            if nlist[j]>nlist[j+1]:
-                nlist[j], nlist[j+1] = nlist[j+1], nlist[j]
-
+    @staticmethod
+    def bubble_sort(nlist):
+        for i in range(len(nlist)):
+            for j in range(0,len(nlist)-i-1):
+                if nlist[j]>nlist[j+1]:
+                    nlist[j], nlist[j+1] = nlist[j+1], nlist[j]
+        return nlist
 
--- test/calcBMI_test.py
+++ test/calcBMI_test.py
@@ -1,16 +1,23 @@
 import unittest
-from src.calcBMI import *
+from src.calcBMI import BMI
 
 class bmitest(unittest.TestCase):
-    def test_bmicalc(self):
+    def test_bubbleSort(self):
         # stub
-        
+        list1=[9,8,7,6,5,4,3,2,1]
 
         # assume
 
+        list1sorted=[1,2,3,4,5,6,7,8,9]
 
         # action
 
+        result1=BMI.bubble_sort(list1)
 
         # expect/assert
 
+        self.assertEqual(result1,list1sorted)
+
+
+if __name__=='__main__':
+    unittest.main()
